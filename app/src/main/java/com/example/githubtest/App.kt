43fe81ef86b5424e01b8bf0aec9bb45af4ff95d4
serhio <serhiooreh@gmail.com>
2023-08-09 09:27:45 +0300

    aa main1

--- app/src/main/java/com/example/githubtest/App.kt
+++ app/src/main/java/com/example/githubtest/App.kt
@@ -3,7 +3,7 @@ package com.example.githubtest
 class App {
     val main2: Int = 0
 
-    val mainNewDay1 = 0
+    val mainNewDay1 = 1000
     val mainNewDay2 = 0
     val editNewDay3 = 0
     val conflictMain2 = 1
